c14afdedd7f4610312ad3ea2feb52758bc8cb706
Lymeng <Lymengchun@gmail.com>
2020-12-22 14:57:54 +0700

    fg

--- jquery/app.js
+++ jquery/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
     console.log('Hello World');
 });
+
+$(".form_datetime").datetimepicker({
+    format: "dd MM yyyy - hh:ii",
+    autoclose: true,
+    todayBtn: true,
+    pickerPosition: "bottom-left"
+});
